d879e2ed99d20bb3d75bb5007cd23cb31a100ed2
howtocode-shizu <151019631+AizDev-Bot@users.noreply.github.com>
2024-05-18 06:15:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -129,9 +129,9 @@ global.loading = require("./utils/log.js");
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/includes/cover/index.html"));
 });
-app.listen(2024, () => {
+app.listen(1000, () => {
   global.loading.log(
-    `Bot is running on port: 2024`,
+    `Bot is running on port: 1000`,
     "SYSTEM",
   );
 });
